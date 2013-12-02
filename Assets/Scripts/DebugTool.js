37aea574be7601d0171d4dc97ca056d5616e33b4
Shigerello <shigerello@gmail.com>
2013-12-02 15:35:54 +0900

    オブジェクト追跡およびデバッグ・メッセージ表示スクリプトがほぼ完成；Follow関数における状態遷移（スナップ<=>ノーマル）の記述を整理、試験的にRaycastを用いた「カメラの壁の背面めり込み防止」コードを追加。

---
+++ Assets/Scripts/DebugTool.js
@@ -0,0 +1,50 @@
+#pragma strict
+
+import System.Collections.Generic;
+
+// Unityコンポーネントとしてアクセスできる変数の必要条件はUnityのObjectクラスを継承していること。C#
+// のDictionaryはそれを満たしていないので、UnityのObjectでラッピングする事で外部にUnityコンポーネン
+// トとして認識させる。
+// 又、表示させる値の型の違いをvarは吸収しない（var foo : SomeClassにOtherClass型の値を代入するとエ
+// ラーが出るとか、JSじゃなくまさしくC#の挙動）ので、表示させたい値を返り値とする匿名関数（Function
+// 型）を代わりに渡す。
+// GenericなクラスはUnityScriptだと記述できないみたいなので、そこはC#で書かないとダメっぽい。
+//
+// !!! UnityScript & C# 混在コード !!!
+public class UnityDictionary extends Object
+{
+    public var dictionary : Dictionary.<String, Function> = new Dictionary.<String, Function>();
+    
+    function SafeQuery(query : String) : Function
+    {
+        // 存在しないキーには空文字列を返り値とする関数を渡す（NullReferenceException対策）。
+        return dictionary.ContainsKey(query) ? dictionary[query] : function() {return "";};
+    }
+};
+
+var watchVars : UnityDictionary = new UnityDictionary();
+
+private var scrollPosition = Vector2.zero;
+
+function Awake() {
+    watchVars.dictionary["Screen rect"] = function() {
+        return "(" + Screen.width + ", " + Screen.height + ")";
+    };
+}
+
+function OnGUI() {
+    DisplayWatchVars();
+}
+
+function DisplayWatchVars() {
+    var message : String;
+    for (var key in watchVars.dictionary.Keys) {
+        message += key + ": " + (watchVars.dictionary[key] as Function)()+ "\n";
+    }
+    
+    scrollPosition = GUILayout.BeginScrollView(scrollPosition, GUILayout.Width(Screen.width), GUILayout.Height(Screen.height*0.1));
+    
+    GUILayout.Label(message);
+    
+    GUILayout.EndScrollView();
+}
--- Assets/Scripts/FollowingObject.js
+++ Assets/Scripts/FollowingObject.js
@@ -4,11 +4,11 @@
 // フォロワーのRigidbody（このスクリプトがアタッチされたオブジェクトのRigidbody）
 //var rigidbody : Rigidbody;
 
-// ターゲットのTransform
-var _TargetTransform : Transform;
+// ターゲットのGameObject
+var _TargetGameObject : GameObject;
 
 // ターゲットのローカル座標（ワールド座標ではない）におけるターゲットからフォロワーへの距離の目標値
-var _Displace : Vector3 = Vector3(0,4,-7.0);
+var _Displace : Vector3 = Vector3(0,6*0.9,-18*0.9);
 
 var _AngularSmoothLag = 0.3;
 var _AngularMaxSpeed = 15.0;
@@ -18,47 +18,66 @@ var _SnapAngularSmoothLag = 0.001;
 var _SnapAngularMaxSpeed = 10000;
 var _SnapDisplaceSmoothLag = 0.001;
 
-var _ClampHeadPositionScreenSpace = 0.75;
-
-var _LockCameraTimeout = 0.2;
-
-private var targetCollider : Collider;
-
+private var targetCollider : Collider; // 参照渡し
 private var angleVelocity = 0.0;
 private var xVelocity = 0.0;
 private var yVelocity = 0.0;
 private var zVelocity = 0.0;
-private var snap = false;
 
-// 追跡を行う関数。値として関数Follow_Snap（目的位置に即座に行く）かFollow_Normal（目的位置にスムー
-// スに行く）をとる。
-private var Follow : Function;
+// 向きや位置を目標値に漸近的に変更する関数。
+private var SmoothDampAngle : Function;
+private var SmoothDamp : Function;
 
-// デバッグ用
-var lineRenderer : LineRenderer;
+#if UNITY_DEBUG // デバッグ用
+private var watchVars : UnityDictionary;
+private var lineMarble : LineRenderer;
+private var lineFollower : LineRenderer;
+function DebugDirection(targetAngle : float, targetQuanternion : Quaternion) {
+    lineMarble.SetPosition(0, _TargetGameObject.transform.position);
+    lineMarble.SetPosition(1, _TargetGameObject.transform.position + _TargetGameObject.rigidbody.velocity.normalized*10);
+    
+    lineFollower.SetPosition(0, _TargetGameObject.transform.position);
+    lineFollower.SetPosition(1, _TargetGameObject.transform.position + (targetQuanternion*_Displace));
+}
+#endif
 
 function Awake ()
 {
-    lineRenderer = gameObject.AddComponent(LineRenderer);
-    lineRenderer.material = new Material (Shader.Find("Particles/Additive"));
-    lineRenderer.SetColors(Color.red, Color.yellow);
-    lineRenderer.SetWidth(0.2, 0.2);
-    lineRenderer.SetVertexCount(2);
+#if UNITY_DEBUG // デバッグ用
+    watchVars = GameObject.Find("DebugTool").GetComponent(DebugTool).watchVars;
+    
+    var mat = new Material(Shader.Find("Particles/Additive"));
+    
+    var lineMarbleGameObject : GameObject = GameObject();
+    lineMarble = lineMarbleGameObject.AddComponent(LineRenderer);
+    lineMarble.material = mat;
+    lineMarble.SetColors(Color.red, Color.yellow);
+    lineMarble.SetWidth(0.2, 0.2);
+    lineMarble.SetVertexCount(2);
+    
+    var lineFollowerGameObject : GameObject = GameObject();
+    lineFollower = lineFollowerGameObject.AddComponent(LineRenderer);
+    lineFollower.material = mat;
+    lineFollower.SetColors(Color.blue, Color.cyan);
+    lineFollower.SetWidth(0.2, 0.2);
+    lineFollower.SetVertexCount(2);
+#endif
     
 	if (!transform) {
 		Debug.Log("Please attach the FollowingCamera script to a follower object.");
 		enabled = false;
 	}
 	
-	if (_TargetTransform)
+	if (_TargetGameObject && _TargetGameObject.collider && _TargetGameObject.collider)
 	{
-		targetCollider = _TargetTransform.collider;
+		targetCollider = _TargetGameObject.collider;
 	}
 	else
-		Debug.Log("Please assign a target object to the FollowingCamera script.");
+    Debug.Log("Please assign a target GameObject with a collider and rigidbody to the FollowingCamera script.");
     
-    // スナッピング：即座にフォロワーの向きをターゲットの向きに合わせる。
-    Follow = Follow_Snap;
+    // スナッピング：即座にフォロワーの向きや位置を目標値に合わせる。
+    SmoothDampAngle = SmoothDampAngleSnapping;
+    SmoothDamp = SmoothDampSnapping;
 	Follow();
 }
 
@@ -66,45 +85,59 @@ function FixedUpdate() {
 	Follow();
 }
 
-function Follow_Snap()
+function Follow()
 {
 	// ターゲットが存在しなければ処理を切り上げる。
-	if (!_TargetTransform)
-		return;
+	if (!_TargetGameObject)
+    return;
     
     //	// Fire2 (alt) キーが押されていたら、フォロワーをスナッピングする。
     //	if (Input.GetButton("Fire2"))
-    //		Follow = Follow_Snap;
+    //    {
+    //        SmoothDampAngle = SmoothDampAngleSnapping;
+    //        SmoothDamp = SmoothDampSnapping;
+    //    }
     
     // ターゲットとフォロワーの縦軸（y軸）周りの回転（進行方向）を取得。
-	var targetAngle = _TargetTransform.eulerAngles.y;
+    var targetQuanternion : Quaternion = Quaternion.FromToRotation(Vector3.forward, _TargetGameObject.rigidbody.velocity);
+    var targetAngle = targetQuanternion.y;
 	var currentAngle = transform.eulerAngles.y;
     
-//    var test : Quaternion = Quaternion.Euler (0, currentAngle, 0);
-//    Gizmos.DrawLine(_TargetTransform.position, _TargetTransform.position + test*Vector3.forward*10);
-    lineRenderer.SetPosition(0, _TargetTransform.position);
-    lineRenderer.SetPosition(1, _TargetTransform.position + Quaternion.Euler (0, currentAngle, 0)*Vector3.forward*10);
+#if UNITY_DEBUG // デバッグ用
+    DebugDirection(targetAngle, targetQuanternion);
+#endif
     
     // フォロワーの前進方向をターゲットの前進方向に漸近させる。
-    currentAngle = Mathf.SmoothDampAngle(currentAngle, targetAngle, angleVelocity, _SnapAngularSmoothLag, _SnapAngularMaxSpeed);
+    currentAngle = SmoothDampAngle(currentAngle, targetAngle);
     
-    // ターゲットとフォロワーの向きがほぼ同じになったので、スナッピングを停止。
-    if (AngleDistance (currentAngle, targetAngle) < 3.0)
-        Follow = Follow_Normal;
+#if UNITY_DEBUG // デバッグ用
+    watchVars.dictionary["currentAngle"] = function() {return currentAngle;};
+#endif
     
     // ターゲットの現在位置を取得。
     var targetPosition : Vector3 = targetCollider.bounds.center;
     // フォロワーの位置を取得。
 	var currentPosition = transform.position;
+    var nextPosition : Vector3;
     
     // ターゲットの向いている方向（currentAngleの向き）を前方にみたてたローカル座標において、
     // ターゲットの現在位置から_Displaceだけずれた位置をフォロワーの目標位置とする。
-    targetPosition += Quaternion.Euler (0, currentAngle, 0) * _Displace;
+    nextPosition = targetPosition + targetQuanternion * _Displace;
+    
+    // 壁への衝突（フォロワーとターゲットの直線上間に障害物）があれば、その衝突した部分にフォロワー
+    // を置く。
+    var wallHit : RaycastHit;
+    if (Physics.Linecast(targetPosition, nextPosition, wallHit))
+    {
+        Debug.DrawRay(wallHit.point, wallHit.normal, Color.red);
+        nextPosition = Vector3(wallHit.point.x, nextPosition.y, wallHit.point.z);
+    }
     
     // フォロワーの位置をフォロワーの目標位置に漸近させる。
-    currentPosition.x = Mathf.SmoothDamp (currentPosition.x, targetPosition.x, xVelocity, _SnapDisplaceSmoothLag);
-    currentPosition.y = Mathf.SmoothDamp (currentPosition.y, targetPosition.y, yVelocity, _SnapDisplaceSmoothLag);
-    currentPosition.z = Mathf.SmoothDamp (currentPosition.z, targetPosition.z, zVelocity, _SnapDisplaceSmoothLag);
+    currentPosition = SmoothDamp(currentPosition, nextPosition);
+#if UNITY_DEBUG // デバッグ用
+    watchVars.dictionary["currentPosition"] = function() {return currentPosition;};
+#endif
     
     // Transformをフォロワーに適用。
 	transform.position = currentPosition;
@@ -113,47 +146,41 @@ function Follow_Snap()
 	transform.LookAt(targetCollider.bounds.center);
 }
 
-function Follow_Normal()
+function SmoothDampAngleSnapping(currentAngle, targetAngle)
 {
-	// ターゲットが存在しなければ処理を切り上げる。
-	if (!_TargetTransform)
-		return;
+    currentAngle = Mathf.SmoothDampAngle(currentAngle, targetAngle, angleVelocity, _SnapAngularSmoothLag, _SnapAngularMaxSpeed);
     
-    //	// Fire2 (alt) キーが押されていたら、フォロワーをスナッピングする。
-    //	if (Input.GetButton("Fire2"))
-    //		Follow = Follow_Snap;
+    // ターゲットとフォロワーの向きがほぼ同じになったので、スナッピングを停止。
+    if (AngleDistance(currentAngle, targetAngle) < 3.0)
+    {
+        SmoothDampAngle = SmoothDampAngleNormal;
+        SmoothDamp = SmoothDampNormal;
+    }
     
-    // ターゲットとフォロワーの縦軸（y軸）周りの回転（進行方向）を取得。
-	var targetAngle = _TargetTransform.eulerAngles.y;
-	var currentAngle = transform.eulerAngles.y;
-	
-//    var test : Quaternion = Quaternion.Euler (0, currentAngle, 0);
-//    Gizmos.DrawLine(_TargetTransform.position, _TargetTransform.position + test*Vector3.forward*10);
-    lineRenderer.SetPosition(0, _TargetTransform.position);
-    lineRenderer.SetPosition(1, _TargetTransform.position + Quaternion.Euler (0, currentAngle, 0)*Vector3.forward*10);
+    return currentAngle;
+}
+
+function SmoothDampAngleNormal(currentAngle, targetAngle)
+{
+    return Mathf.SmoothDampAngle(currentAngle, targetAngle, angleVelocity, _AngularSmoothLag, _AngularMaxSpeed);
+}
+
+function SmoothDampSnapping(currentPosition : Vector3, targetPosition : Vector3)
+{
+    currentPosition.x = Mathf.SmoothDamp(currentPosition.x, targetPosition.x, xVelocity, _SnapDisplaceSmoothLag);
+    currentPosition.y = Mathf.SmoothDamp(currentPosition.y, targetPosition.y, yVelocity, _SnapDisplaceSmoothLag);
+    currentPosition.z = Mathf.SmoothDamp(currentPosition.z, targetPosition.z, zVelocity, _SnapDisplaceSmoothLag);
     
-    // フォロワーの前進方向をターゲットの前進方向に漸近させる。
-    currentAngle = Mathf.SmoothDampAngle(currentAngle, targetAngle, angleVelocity, _AngularSmoothLag, _AngularMaxSpeed);
+    return currentPosition;
+}
+
+function SmoothDampNormal(currentPosition : Vector3, targetPosition : Vector3)
+{
+    currentPosition.x = Mathf.SmoothDamp(currentPosition.x, targetPosition.x, xVelocity, _DisplaceSmoothLag);
+    currentPosition.y = Mathf.SmoothDamp(currentPosition.y, targetPosition.y, yVelocity, _DisplaceSmoothLag);
+    currentPosition.z = Mathf.SmoothDamp(currentPosition.z, targetPosition.z, zVelocity, _DisplaceSmoothLag);
     
-    // ターゲットの現在位置を取得。
-    var targetPosition : Vector3 = targetCollider.bounds.center;
-    // フォロワーの位置を取得。
-	var currentPosition = transform.position;
-    
-    // ターゲットの向いている方向（currentAngleの向き）を前方にみたてたローカル座標において、
-    // ターゲットの現在位置から_Displaceだけずれた位置をフォロワーの目標位置とする。
-    targetPosition += Quaternion.Euler (0, currentAngle, 0) * _Displace;
-    
-    // フォロワーの位置をフォロワーの目標位置に漸近させる。
-    currentPosition.x = Mathf.SmoothDamp (currentPosition.x, targetPosition.x, xVelocity, _DisplaceSmoothLag);
-    currentPosition.y = Mathf.SmoothDamp (currentPosition.y, targetPosition.y, yVelocity, _DisplaceSmoothLag);
-    currentPosition.z = Mathf.SmoothDamp (currentPosition.z, targetPosition.z, zVelocity, _DisplaceSmoothLag);
-    
-    // Transformをフォロワーに適用。
-	transform.position = currentPosition;
-	
-	// フォロワーをターゲットの中心に向ける
-	transform.LookAt(targetCollider.bounds.center);
+    return currentPosition;
 }
 
 function AngleDistance (a : float, b : float)
